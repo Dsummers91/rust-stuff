9ae05958f5f1aa7993773db04dc1076100a10ddd
dsummers91 <deonsummers91@gmail.com>
2017-11-01 08:00:04 -0500

    add worker fmt

--- store/src/main.rs
+++ store/src/main.rs
@@ -1,11 +1,14 @@
 #![feature(vec_remove_item)]
 
+use std::fmt::{Display, Formatter, Result};
+
+
 struct Store{
   name: &'static str,
   workers: Vec<Worker>,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 struct Worker{
   name: &'static str, 
   age: i32
@@ -13,23 +16,30 @@ struct Worker{
 
 impl Store {
   fn hire_worker(&mut self, worker: Worker) {
-    let Worker{name, age} = worker;
     self.workers.push(worker);
-    println!("{} ({}) added!", name, age);
+    println!("{} added!", worker);
   }
 
   fn fire_worker(&mut self, worker: Worker) {
     self.workers.remove_item(&worker);
-    println!("{} removed", worker.name);
+    println!("{} removed", worker);
   }
+}
+
+impl Display for Worker {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{} ({})", self.name, self.age)
+    }
 }
 
 fn main() {
   let mut store = Store{name:"test", workers:Vec::new()};
+  let bob = Worker{name:"Bob", age:18};
   println!("{}", store.name);
   store.hire_worker(Worker{name:"Mary", age:16});
-  store.hire_worker(Worker{name:"Bob", age:18});
+  store.hire_worker(bob);
   println!("{:?}", store.workers);
-  store.fire_worker(Worker{name:"Bob", age:18});
+  println!("{}", bob);
+  store.fire_worker(bob);
   println!("{:?}", store.workers);
 }
