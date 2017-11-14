f144360e13f1a26ddd30355b3162c91746b9e3b0
dsummers91 <deonsummers91@gmail.com>
2017-11-14 02:29:33 -0600

    Add mutex locking and lifetimes to workers

--- store/src/main.rs
+++ store/src/main.rs
@@ -1,29 +1,32 @@
 #![feature(vec_remove_item)]
 
+use std::sync::{Arc, Mutex};
 use std::fmt::{Display, Formatter, Result};
 
 
-struct Store{
-  name: &'static str,
-  workers: Vec<Worker>,
+struct Store<'a> {
+    name: &'static str,
+    workers: Mutex<&'a mut Vec<Worker>>,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
-struct Worker{
-  name: &'static str, 
-  age: i32
+struct Worker {
+    name: &'static str,
+    age: i32,
 }
 
-impl Store {
-  fn hire_worker(&mut self, worker: Worker) {
-    self.workers.push(worker);
-    println!("{} added!", worker);
-  }
+impl<'a> Store<'a> {
+    fn hire_worker(&'a self, worker: Worker) {
+        let mut workers = self.workers.lock().unwrap();
+        workers.push(worker);
+        println!("{} added!", worker);
+    }
 
-  fn fire_worker(&mut self, worker: Worker) {
-    self.workers.remove_item(&worker);
-    println!("{} removed", worker);
-  }
+    fn fire_worker(&'a self, worker: Worker) {
+        let mut workers = self.workers.lock().unwrap();
+        workers.remove_item(&worker);
+        println!("{} removed", worker);
+    }
 }
 
 impl Display for Worker {
@@ -33,13 +36,25 @@ impl Display for Worker {
 }
 
 fn main() {
-  let mut store = Store{name:"test", workers:Vec::new()};
-  let bob = Worker{name:"Bob", age:18};
-  println!("{}", store.name);
-  store.hire_worker(Worker{name:"Mary", age:16});
-  store.hire_worker(bob);
-  println!("{:?}", store.workers);
-  println!("{}", bob);
-  store.fire_worker(bob);
-  println!("{:?}", store.workers);
+    let mut workers = Vec::new();
+    let mut store = Store {
+        name: "test",
+        workers: Mutex::new(&mut workers),
+    };
+    let bob = Worker {
+        name: "Bob",
+        age: 18,
+    };
+    println!("{}", store.name);
+    store.hire_worker(Worker {
+        name: "Mary",
+        age: 16,
+    });
+    store.hire_worker(bob);
+
+    println!("{:?}", store.workers);
+    println!("{}", bob);
+    store.fire_worker(bob);
+    store.fire_worker(bob);
+    println!("{:?}", store.workers);
 }
